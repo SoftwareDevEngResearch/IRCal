8374349b738690ac168da05f8a56808cf7f8858a
Derek Bean <beande@regonstate.edu>
2018-04-23 15:39:02 -0700

    Finished the first iterations of the tests for the functions. the second fucntion compile_file does not work but the test does. The tests need a lot of expansion for completeness but I am not sure what edge tests would be for these cases.

--- tests/test_BC.py
+++ tests/test_BC.py
@@ -8,6 +8,7 @@ Created on Mon Apr 23 11:34:50 2018
 
 from IRCal.tools.blackbody_connect import BlackBody_Control as BC
 import pytest
+import os
 
 def test_write_file():
     ''' Test to evaluate the write file function. 
@@ -24,14 +25,27 @@ def test_write_file():
                 text.append(line)
         return text
     
-    test1 = BC()
-    fpath = test1.write_file(file_lines(filename))
+    write_test = BC()
+    fpath = write_test.write_file(file_lines(filename))
     assert all(i == j for i, j in zip(file_lines(filename), file_lines(fpath)))
     
     
 def test_compile_file():
-    a = 0
-    
+    ''' Test the fuctionality of the compile file function to see if it
+        compiles the ouput file without errors. The success of the compile
+        file function also relies on the correct inputs to the write file
+        fuction which will be implemented in the future'''
+    test_path = os.path.expanduser('~/Documents/SoftwareClass/IRCal/IRCal/tests/test_files')
+#    test_command = "gcc -o hello hello_world.c"
+    compile_test = BC()
+#    os.chdir(test_path)
+    commands = ["gcc",  "-o", "hello", "hello_world.c"]
+    compile_test.compile_file(commands)
+    assert os.path.isfile(os.path.join(test_path, 'hello')) == True
+        
 def test_execute_file():
-    a = 0
+    program_name = os.path.expanduser('~/Documents/SoftwareClass/IRCal/IRCal/tests/test_files/hello')
+    execute_test = BC()
+    output = execute_test.run_program(program_name)
+    assert output.strip() == b'hello, world'
     
