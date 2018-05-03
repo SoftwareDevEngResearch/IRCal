3dba3fa90e42f6a91a157dfd61ec15c84a084afd
Derek Bean <derekrbean@gamil.com>
2018-05-02 20:56:15 -0700

    Reoved the unused control_file.c and added a skeleton class for image analysis called image_analysis.py that will be used to perform the image processing operations. This will likely need to use opencv (cv2 in python) as it has a built in ROI function among other usefl tools

---
+++ tools/image_analysis.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Python script for conducting image analysis on infrared images
+@author: Derek Bean
+"""
+
+class Image_Tools():
+    def __init__(self):
+        return None
+
+    def img_rms(self):
+        return None
+
+    def define_roi(self):
+        return None
+
+    def background_subratction(self):
+        return None
