22882ced59dfa174c23edb75fb38d2bda4d46066
Derek Bean <beande@regonstate.edu>
2018-05-21 14:56:32 -0700

    Worked on some image tools for reading the datasets into numpy arrays

--- development_file.py
+++ development_file.py
@@ -10,7 +10,10 @@ import os
 
 if __name__ == '__main__':
     sfmov_path = os.path.join(os.path.abspath('.'), 'test/test_files/')
-    file_name = "B-lab test-000002"
-    se = sc.SfmovTools(sfmov_path, sfmov_path, file_name)
-    print(se.camera_name)
-    se.convert()
+    file_name = "ir_test_file"
+    # se = sc.SfmovTools(sfmov_path, sfmov_path, file_name)
+    # print(se.camera_name)
+    # se.convert(compression_factor=0)
+    test_image = ia.Image_Tools(sfmov_path, file_name)
+    test_image.get_attributes()
+    data = test_image.read_frames()
--- tools/image_analysis.py
+++ tools/image_analysis.py
@@ -6,9 +6,58 @@ Python script for conducting image analysis on infrared images
 @author: Derek Bean
 """
 
+import h5py
+import os
+
+
 class Image_Tools():
-    def __init__(self):
-        return None
+    def __init__(self, file_path, file_name):
+        self.file_path = self.path_handling(file_path)
+        self.file_name = file_name
+        self.data_attributes = {}
+
+    @staticmethod
+    def path_handling(path):
+        """ TODO: Need to remove and replace function with os package for path handling across operating systems"""
+        path.replace('\\', '/')
+        if path[0] == '/':
+            path = '/' + path
+        return path
+
+
+    def open_hdf5(self):
+        """
+        Open and return a file object based on the input path
+
+        Input:
+        extension: a string with the
+        """
+        return h5py.File(os.path.join(self.file_path, self.file_name + '.hdf5'), 'r')
+
+    def get_attributes(self):
+        """
+        Get the values for important parameters from the hdf5 file such as dropped frames and the frame rate
+
+        :returns a dictionary of the important attributes with the keys the same as those in the hdf5 file
+        """
+        data = {}
+        with self.open_hdf5() as file:
+            for key in file.keys():
+                if 'data' not in key.lower():
+                    data[key] = file.get(key)[()]
+        self.data_attributes = data
+        return self.data_attributes
+
+    def read_frames(self, frame_number=-1):
+        try:
+            number_of_frames = self.data_attributes['number_of_frames']
+        except NameError:
+            self.get_attributes()
+            self.read_frames(self, frame_number=frame_number)
+        with self.open_hdf5() as file:
+            frame_dataset = file.get('data')[()]
+            print(frame_dataset)
+        return frame_dataset
 
     def img_rms(self):
         return None
